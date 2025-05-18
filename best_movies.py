71c5fc9a66a5f843c3d4713fa6db62d7178ef8b0
Evgenii Bulychev <zhindozz@yandex.ru>
2025-05-18 15:07:16 +0300

    Added script with best films

---
+++ best_movies.py
@@ -0,0 +1,4 @@
+best_movies = ['Pobeg from Showshank', 'Crime chtivo', 'Forrest Gamp', 'Leon', 'King Lion'] 
+for movie in best_movies:
+    print(movie)
+print('I didt go to film school, i went movies.')
